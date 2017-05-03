3539c604d33b7e0c65e4d0906be492f00a0619d3
mochalygin <a@mochalygin.ru>
2017-05-03 17:39:36 +0300

    ADD Array(Date) Type class

---
+++ src/Types/ArrayDateType.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * This file is part of the FODDBALClickHouse package -- Doctrine DBAL library
+ * for ClickHouse (a column-oriented DBMS for OLAP <https://clickhouse.yandex/>)
+ *
+ * (c) FriendsOfDoctrine <https://github.com/FriendsOfDoctrine/>.
+ *
+ * For the full copyright and license inflormation, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOD\DBALClickHouse\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+/**
+ * Array(Date) Type class
+ *
+ * @author Mochalygin <a@mochalygin.ru>
+ */
+class ArrayDateType extends ArrayType
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'Array(Date)';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'array(date)';
+    }
+}
