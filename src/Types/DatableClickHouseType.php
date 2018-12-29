4bf04ef6c8404cc85ed326bfa0adacba4aff83c4
Philip <filippF@bk.ru>
2018-12-29 15:32:47 +0300

    Fixed spelling

--- src/Types/DatableClickHouseType.php
+++ src/Types/DatableClickHouseType.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  *
  * (c) FriendsOfDoctrine <https://github.com/FriendsOfDoctrine/>.
  *
- * For the full copyright and license inflormation, please view the LICENSE
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
